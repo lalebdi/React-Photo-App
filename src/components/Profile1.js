e739ba27c5144f6874be7e6af76ca2ea0e7c28e1
lalebdi <loai.alebdi@gmail.com>
2020-07-23 14:20:21 -0400

    added the class components

---
+++ src/components/Profile1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Profile1 extends Component {
+    render() {
+        return (
+            <div className="side-profile">
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/Profile3.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Profile3 extends Component {
+    render() {
+        return (
+            <div className="side-profile">
+                
+            </div>
+        )
+    }
+}
