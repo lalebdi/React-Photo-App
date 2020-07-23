834c82ee2be4be89afc24708f89af81b54cef04b
lalebdi <loai.alebdi@gmail.com>
2020-07-23 15:21:45 -0400

    centered the card deck

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,38 +17,39 @@ export default class Home extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="home-big-wrapper">
                 <NavBar />
+                <div className="profile-cards">
                 <CardDeck>
-      <Card>
-        <CardImg top width="100%" src={stewieProfilePic} alt="Stewie Profile Picture" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src={QuagmireProfilePic} alt="Quagmire Profile Picture" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src={BrianProfilePic} alt="Brian Profile Picture" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </CardDeck>
-                
+                    <Card>
+                        <CardImg top width="50%" src={stewieProfilePic} alt="Stewie Profile Picture" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                            <Button>Login</Button>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <CardImg top width="50%" src={QuagmireProfilePic} alt="Quagmire Profile Picture" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                            <Button>Login</Button>
+                        </CardBody>
+                    </Card>
+                    <Card>
+                        <CardImg top width="50%" src={BrianProfilePic} alt="Brian Profile Picture" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                            <Button>Login</Button>
+                        </CardBody>
+                    </Card>
+                    </CardDeck>
+                </div>
             </div>
         )
     }
