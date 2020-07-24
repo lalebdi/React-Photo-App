ce3670acb726f0a1f1804585b2b00a48cea10356
lalebdi <loai.alebdi@gmail.com>
2020-07-24 11:45:11 -0400

    spaced out the bars

--- src/components/Profile1.js
+++ src/components/Profile1.js
@@ -6,9 +6,13 @@ export default class Profile1 extends Component {
         return (
             <div className="side-profile">
                 <ListGroup>
+                    <br/>
                     <ListGroupItem color="success">profile photo here</ListGroupItem>
+                    <br/>
                     <ListGroupItem color="info">the user name here</ListGroupItem>
+                    <br/>
                     <ListGroupItem color="warning">number of posts here</ListGroupItem>
+                    <br/>
                     <ListGroupItem color="danger">number of followers here</ListGroupItem>
                 </ListGroup>
             </div>
