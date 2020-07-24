21c190dd7987c16ad38a515eb79001bd49bbad82
lalebdi <loai.alebdi@gmail.com>
2020-07-24 11:37:50 -0400

    improved the layout here

--- src/components/Profile1.js
+++ src/components/Profile1.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class Profile1 extends Component {
     render() {
         return (
             <div className="side-profile">
-                
+                <ListGroup>
+                    <ListGroupItem color="success">profile photo here</ListGroupItem>
+                    <ListGroupItem color="info">the user name here</ListGroupItem>
+                    <ListGroupItem color="warning">number of posts here</ListGroupItem>
+                    <ListGroupItem color="danger">number of followers here</ListGroupItem>
+                </ListGroup>
             </div>
         )
     }
