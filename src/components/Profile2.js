9923c18f93b5079eecb82beab934a051d14be7bc
lalebdi <loai.alebdi@gmail.com>
2020-07-24 11:46:33 -0400

    spcaed out the bars

--- src/components/Profile2.js
+++ src/components/Profile2.js
@@ -6,9 +6,13 @@ export default class Profile2 extends Component {
         return (
             <div className="side-profile">
                 <ListGroup>
+                    <br/>
                     <ListGroupItem color="success">Profile photo here</ListGroupItem>
+                    <br/>
                     <ListGroupItem color="info">insert the user name here</ListGroupItem>
+                    <br/>
                     <ListGroupItem color="warning">insert the number of posts here</ListGroupItem>
+                    <br/>
                     <ListGroupItem color="danger">insert the number of followers here</ListGroupItem>
                 </ListGroup>
             </div>
