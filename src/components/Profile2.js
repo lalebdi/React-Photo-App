ab1fc45ffc78c7c66ea9f924de4050e627bb101c
lalebdi <loai.alebdi@gmail.com>
2020-07-24 11:34:01 -0400

    added the bars for the user info

--- src/components/Profile2.js
+++ src/components/Profile2.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class Profile2 extends Component {
     render() {
         return (
             <div className="side-profile">
-                
+                <ListGroup>
+                    <ListGroupItem color="success">Cras justo odio</ListGroupItem>
+                    <ListGroupItem color="info">Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem color="warning">Morbi leo risus</ListGroupItem>
+                    <ListGroupItem color="danger">Porta ac consectetur ac</ListGroupItem>
+                </ListGroup>
             </div>
         )
     }
